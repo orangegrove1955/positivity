7a44cd922f644b91c2ed81d9a102c2b575c079dd
orangegrove_1955 <orangegrove_1955@hotmail.com>
2020-04-08 22:19:34 +1000

    Added heart loading animation

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const addMessage = () => {
   console.log("adding message");
 };
+
+const hearts = () => {
+  let hearts = document.getElementById("message");
+
+  let heartcount = (hearts.offsetWidth / 50) * 5;
+  for (let i = 0; i <= heartcount; i++) {
+    const size = rnd(60, 120) / 10;
+    let particle = document.createElement("span");
+    particle.setAttribute("class", "particle");
+    particle.setAttribute(
+      "style",
+      `top:${rnd(20, 80)}%; 
+          left: ${rnd(0, 95)}%;
+          width: ${size}px;
+          height: ${size}px;
+          animation-delay: ${rnd(0, 30) / 10}s;`
+    );
+    hearts.appendChild(particle);
+  }
+};
+
+const rnd = (m, n) => {
+  m = parseInt(m);
+  n = parseInt(n);
+  return Math.ceil(Math.random() * (n - m + 1)) + m;
+};
+
+document.onload = hearts();
