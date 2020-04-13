18ec3b6c5572de06d59af5f9b6b4b2c071b11d61
orangegrove_1955 <orangegrove_1955@hotmail.com>
2020-04-13 13:38:41 +1000

    Only returning saying of random true values

--- functions/randomSaying.js
+++ functions/randomSaying.js
@@ -10,34 +10,30 @@ const client = new faunadb.Client({
 exports.handler = (event, context, callback) => {
   console.log("Function `random-saying` invoked");
 
-  return (
-    client
-      .query(
-        q.Map(
-          q.Paginate(q.Match(q.Index("sayingsByApprovedFlag"), false)),
-          q.Lambda("sayings", q.Get(q.Var("sayings")))
-        )
+  return client
+    .query(
+      q.Map(
+        q.Paginate(q.Match(q.Index("sayingsByApprovedFlag"), true)),
+        q.Lambda("sayings", q.Get(q.Var("sayings")))
       )
-      // q.Get(q.Ref(q.Collection("Saying"), "262464456652489226")))
-      .then((response) => {
-        console.log("Successfully got random saying", response.data);
-        let items = response.data;
+    )
+    .then((response) => {
+      console.log("Successfully got random saying", response.data);
+      let items = response.data;
 
-        let randomValue = Math.floor(Math.random() * items.length);
-        let returnValue = items[randomValue];
+      let randomValue = Math.floor(Math.random() * items.length);
+      let returnValue = items[randomValue];
 
-        return callback(null, {
-          statusCode: 200,
-          // body: JSON.stringify(response.data.saying),
-          body: JSON.stringify(returnValue),
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-        return callback(null, {
-          statusCode: 400,
-          body: "Something went wrong",
-        });
-      })
-  );
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(returnValue.data.saying),
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      return callback(null, {
+        statusCode: 400,
+        body: "Something went wrong",
+      });
+    });
 };
